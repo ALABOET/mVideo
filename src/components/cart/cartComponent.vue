f44502dbd13ce5115d5f2041a8107ebca917d96d
Ivan Tiurin <kennynotch11@gmail.com>
2022-08-02 10:30:41 +0300

    SPEX-14: Additional tweaks to cart + margin adjustments

--- src/components/cart/cartComponent.vue
+++ src/components/cart/cartComponent.vue
@@ -1,107 +1,115 @@
 <template>
-  <div class="sx-cart-component">
-    <div v-if="addedProducts.length > 0" class="sx-cart-component-full">
-      <div class="sx-cart-component-full-title">
-        <span>Товары в корзине: {{ addedProducts.length }}</span>
-        <br/>
-        <router-link to="/catalog">
-          В каталог
-        </router-link>
-        <div class="sx-cart-component-full-title-items" v-for="(elem,index) in addedProducts" :key="elem">
-          <span class="sx-cart-component-full-title-items-item">{{ elem.name }}</span>
-          {{ elem.price }} рублей <span @click="deleteItem(index)"
-                                        class="sx-cart-component-full-title-items-delete">X</span>
+    <div class="sx-cart-component">
+        <div v-if="addedProducts.length > 0" class="sx-cart-component-full">
+            <div class="sx-cart-component-full-title">
+                <span>Товары в корзине: {{ addedProducts.length }}</span>
+                <br/>
+                <router-link to="/catalog">
+                    В каталог
+                </router-link>
+                <div class="sx-cart-component-full-title-items" v-for="(elem,index) in addedProducts" :key="elem">
+                    <span class="sx-cart-component-full-title-items-item">{{ elem.name }}</span>
+                    {{ elem.price }} рублей <span @click="deleteItem(index)"
+                                                  class="sx-cart-component-full-title-items-delete">X</span>
+                </div>
+                <div class="sx-cart-component-full-title-buttons">
+                    <button @click="clearItemsInCart">Очистить корзину</button>
+                    <button @click="purchase">Купить</button>
+                </div>
+                <div class="sx-cart-component-full-title-warning" v-if="isWarningShown">Для покупки необходимо
+                    авторизоваться
+                </div>
+            </div>
         </div>
-        <button @click="clearItemsInCart">Очистить корзину</button>
-        <button @click="purchase">Купить</button>
-        <div class="sx-cart-component-full-title-warning" v-if="isWarningShown">Для покупки необходимо авторизоваться
-        </div>
-      </div>
-    </div>
 
 
-    <div v-else class="sx-cart-component-empty">Что-то тут пусто, вернитесь в
-      <router-link to="/catalog">
-        магазин
-      </router-link>
-      и наполните корзину
+        <div v-else class="sx-cart-component-empty">Что-то тут пусто, вернитесь в
+            <router-link to="/catalog">
+                магазин
+            </router-link>
+            и наполните корзину
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import {mapState, mapMutations} from "vuex";
+    import {mapState, mapMutations} from "vuex";
 
-export default {
-  name: "cartComponent",
-  computed: {
-    ...mapState(['itemsInCart', 'addedProducts', 'isLoggedIn']),
-  },
-  data() {
-    return {
-      isWarningShown: false,
+    export default {
+        name: "cartComponent",
+        computed: {
+            ...mapState(['itemsInCart', 'addedProducts', 'isLoggedIn']),
+        },
+        data() {
+            return {
+                isWarningShown: false,
+            }
+        },
+        methods: {
+            ...mapMutations(['deleteElement', 'completePurchase', 'clearItemsInCart']),
+            deleteItem(item) {
+                this.deleteElement(item);
+            },
+            purchase() {
+                if (this.isLoggedIn) {
+                    this.completePurchase();
+                    this.$router.push('/purchase')
+                } else {
+                    this.isWarningShown = true;
+                }
+            }
+        }
     }
-  },
-  methods: {
-    ...mapMutations(['deleteElement', 'completePurchase', 'clearItemsInCart']),
-    deleteItem(item) {
-      this.deleteElement(item);
-    },
-    purchase() {
-      if (this.isLoggedIn) {
-        this.completePurchase();
-        this.$router.push('/purchase')
-      } else {
-        this.isWarningShown = true;
-      }
-    }
-  }
-}
 </script>
 
 <style lang="scss" scoped>
-.sx-cart-component {
-  font-size: 20px;
-  width: 600px;
+    .sx-cart-component {
+        font-size: 20px;
+        width: 600px;
 
-  &-empty, &-full {
-    a {
-      text-decoration: none;
-      color: red;
+        &-empty, &-full {
+            a {
+                text-decoration: none;
+                color: red;
+            }
+
+            &-title {
+                box-shadow: 0 0 10px black;
+                border-radius: 5px;
+                padding: 10px;
+
+                &-buttons {
+                    display: flex;
+                    gap: 15px;
+                }
+
+                &-items {
+                    position: relative;
+                    margin: 15px 0;
+
+                    &-item {
+                        color: grey;
+                    }
+
+                    &-delete {
+
+
+                        &:hover {
+                            cursor: pointer;
+                        }
+                    }
+                }
+
+                &-warning {
+                    color: red;
+                }
+            }
+        }
     }
 
-    &-title {
-      box-shadow: 0 0 10px black;
-      border-radius: 5px;
-      padding: 10px;
-
-      &-items {
-        position: relative;
-        margin: 15px 0;
-
-        &-item {
-          color: grey;
+    @media screen and (max-width: 650px) {
+        .sx-cart-component {
+            width: 90%;
         }
-
-        &-delete {
-
-
-          &:hover {
-            cursor: pointer;
-          }
-        }
-      }
-
-      &-warning {
-        color: red;
-      }
     }
-  }
-}
-
-@media screen and (max-width: 650px) {
-  .sx-cart-component {
-    width: 90%;
-  }
-}
 </style>
--- src/components/cart/cartDataComponent.vue
+++ src/components/cart/cartDataComponent.vue
@@ -1,129 +1,143 @@
 <template>
-  <div class="sx-cart-data">
-    <div style="font-size: 24px">Оформление заказа</div>
-    <span style="color: red">Сумма заказа - {{ sumOfMoney }} рублей</span>
-    <div>1. Укажите личные данные пользователя</div>
-    <span>ФИО </span>
-    <input type="text" v-model="name">
-    <br/>
-    <span>Дата рождения </span>
-    <input type="date" v-model="birthday">
-    <br/>
-    <span>Адрес доставки </span>
-    <input type="text" v-model="address">
-    <br/>
-    <div>2. Как Вам лучше оформить заказ?</div>
-    <div class="sx-cart-data-options">
-      <div class="sx-cart-data-options-elem" v-for="elem in waysToOrder" :key="elem">
-        <way-to-order-component @onWayClicked="selectOption" :order-type="elem.wayName"
-                                :order-description="elem.wayDescription"
-                                :radio-value="elem.radioValue"
-                                :class="{'sx-cart-data-on':elem.radioValue === this.text}"
-                                :is-selectable="elem.wayName === 'В рассрочку'"
-                                :used-array="bankArray"/>
-      </div>
-    </div>
-    <div>3. Списание бонусов</div>
-    <div class="sx-cart-data-bonus">
-      <input @click="useBonus" type="checkbox" v-model="totalBonuses"><span>Использовать бонусы</span>
-      <div class="sx-cart-data-bonus-scale" :class="{'sx-cart-data-bonus-scale__disabled':!useBonuses}">
-        <div>Сумма</div>
-        <input type="text" v-model="usedBonuses">
+    <div class="sx-cart-data">
+        <div style="font-size: 24px">Оформление заказа</div>
+        <span style="color: red">Сумма заказа - {{ sumOfMoney }} рублей</span>
+        <div>1. Укажите личные данные пользователя</div>
+        <span>ФИО </span>
+        <input type="text" v-model="name">
         <br/>
-        <input type="range" min="0" v-model="usedBonuses" :max="totalBonuses"/>
+        <span>Дата рождения </span>
+        <input type="date" v-model="birthday">
         <br/>
-        <button @click="useCurrentBonuses">Списать бонусы</button>
-      </div>
-      <div v-if="showFinal"> Использовано бонусов: {{ usedBonuses }}
-        <div>Итоговая сумма: {{ sumOfMoney - usedBonuses }} рублей</div>
-      </div>
+        <span>Адрес доставки </span>
+        <input type="text" v-model="address">
+        <br/>
+        <div>2. Как Вам лучше оформить заказ?</div>
+        <div class="sx-cart-data-options">
+            <div class="sx-cart-data-options-elem" v-for="elem in waysToOrder" :key="elem">
+                <way-to-order-component @onWayClicked="selectOption" :order-type="elem.wayName"
+                                        :order-description="elem.wayDescription"
+                                        :radio-value="elem.radioValue"
+                                        :class="{'sx-cart-data-on':elem.radioValue === this.text}"
+                                        :is-selectable="elem.wayName === 'В рассрочку'"
+                                        :used-array="bankArray"/>
+            </div>
+        </div>
+        <div v-if="isPaymentShown">
+            <div>3. Списание бонусов</div>
+            <div class="sx-cart-data-bonus">
+                <input @click="useBonus" type="checkbox" v-model="totalBonuses"><span>Использовать бонусы</span>
+                <div class="sx-cart-data-bonus-scale" :class="{'sx-cart-data-bonus-scale__disabled':!useBonuses}">
+                    <div>Сумма</div>
+                    <input type="text" v-model="usedBonuses">
+                    <br/>
+                    <input type="range" min="0" v-model="usedBonuses" :max="totalBonuses"/>
+                    <br/>
+                    <button @click="useCurrentBonuses">Списать бонусы</button>
+                </div>
+                <div v-if="showFinal"> Использовано бонусов: {{ usedBonuses }}
+                    <div>Итоговая сумма: {{ sumOfMoney - usedBonuses }} рублей</div>
+                </div>
+            </div>
+        </div>
+        <div>4. Завершение покупки</div>
+        <button :class="{'sx-cart-data-bonus-scale__disabled':!isInfoPresent}" @click="finishBuying">{{isPaymentShown? 'Купить товары' : 'Перейти в банк'}}</button>
     </div>
-    <div>4. Завершение покупки</div>
-    <button :class="{'sx-cart-data-bonus-scale__disabled':!isInfoPresent}" @click="finishBuying">Купить товары</button>
-  </div>
 </template>
 
 <script>
-import WayToOrderComponent from "@/components/cart/wayToOrderComponent";
-import {mapState, mapMutations} from "vuex";
+    import WayToOrderComponent from "@/components/cart/wayToOrderComponent";
+    import {mapState, mapMutations} from "vuex";
 
-export default {
-  name: "cartDataComponent",
-  components: {WayToOrderComponent},
-  methods: {
-    ...mapMutations(['setUserBonuses', 'itemsBought']),
-    selectOption(value) {
-      this.text = value
-    },
-    useBonus() {
-      this.useBonuses = !this.useBonuses;
-    },
-    useCurrentBonuses() {
-      this.setUserBonuses(this.usedBonuses)
-      this.useBonuses = false;
-      this.showFinal = true;
-    },
-    finishBuying() {
-      this.$router.push('/catalog');
-      this.itemsBought();
+    export default {
+        name: "cartDataComponent",
+        components: {WayToOrderComponent},
+        methods: {
+            ...mapMutations(['setUserBonuses', 'itemsBought']),
+            selectOption(value) {
+                this.text = value
+                console.log(value)
+            },
+            useBonus() {
+                this.useBonuses = !this.useBonuses;
+            },
+            useCurrentBonuses() {
+                this.setUserBonuses(this.usedBonuses)
+                this.useBonuses = false;
+                this.showFinal = true;
+            },
+            finishBuying() {
+                this.$router.push('/catalog');
+                this.itemsBought();
+            }
+        },
+        computed: {
+            ...mapState(['totalBonuses', 'sumOfMoney']),
+            isInfoPresent() {
+                return this.name !== '' && this.birthday !== null && this.address !== '' && this.text !== ''
+            },
+            isPaymentShown() {
+                return this.text === 'online' || this.text === 'offline' || this.text === ''
+            }
+        },
+        data() {
+            return {
+                name: '',
+                birthday: null,
+                address: '',
+                text: '',
+                useBonuses: false,
+                usedBonuses: 0,
+                showFinal: false,
+                bankArray: ['МТС', 'Сбербанк', 'Россельхозбанк', 'Тинькофф'],
+                waysToOrder: [{
+                    wayName: 'Онлайн', wayDescription: 'Оплатить покупки можно только пластиковой картой.\n' +
+                        'При использовании VPN сервисов могут наблюдаться проблемы.', radioValue: 'online'
+                }, {
+                    wayName: 'При получении',
+                    wayDescription: 'Оплатить покупки при получении товара в магазине.', radioValue: 'offline'
+                }, {wayName: 'В кредит', wayDescription: '', radioValue: 'credit'}, {
+                    wayName: 'В рассрочку',
+                    wayDescription: 'Список банков-партнеров.', radioValue: 'installment'
+                }]
+            }
+        }
     }
-  },
-  computed: {
-    ...mapState(['totalBonuses', 'sumOfMoney']),
-    isInfoPresent() {
-      return this.name !== '' && this.birthday !== null && this.address !== ''
-    }
-  },
-  data() {
-    return {
-      name: '',
-      birthday: null,
-      address: '',
-      text: '',
-      useBonuses: false,
-      usedBonuses: 0,
-      showFinal: false,
-      bankArray: ['МТС', 'Сбербанк', 'Россельхозбанк', 'Тинькофф'],
-      waysToOrder: [{
-        wayName: 'Онлайн', wayDescription: 'Оплатить покупки можно только пластиковой картой.\n' +
-            'При использовании VPN сервисов могут наблюдаться проблемы.', radioValue: 'online'
-      }, {
-        wayName: 'При получении',
-        wayDescription: 'Оплатить покупки при получении товара в магазине.', radioValue: 'offline'
-      }, {wayName: 'В кредит', wayDescription: '', radioValue: 'credit'}, {
-        wayName: 'В рассрочку',
-        wayDescription: 'Список банков-партнеров.', radioValue: 'installment'
-      }]
-    }
-  }
-}
 </script>
 
 <style lang="scss">
-.sx-cart-data {
-  &-bonus {
-    &-scale {
-      &__disabled {
-        opacity: 0.4;
-        pointer-events: none;
-      }
+    .sx-cart-data {
+        &-bonus {
+            &-scale {
+                &__disabled {
+                    opacity: 0.4;
+                    pointer-events: none;
+                }
+            }
+        }
+
+        &-options {
+            display: flex;
+            flex-basis: 50%;
+            flex-wrap: wrap;
+            flex-direction: column;
+            align-items: center;
+
+            &-elem {
+                margin: 10px 10px;
+            }
+        }
     }
-  }
 
-  &-options {
-    display: flex;
-    flex-basis: 50%;
-    flex-wrap: wrap;
-    width: 650px;
-
-    &-elem {
-      margin: 10px 10px;
+    .sx-cart-data-on {
+        box-shadow: 0 0 10px greenyellow;
     }
-  }
-}
 
-.sx-cart-data-on {
-  box-shadow: 0 0 10px greenyellow;
-}
+    @media screen and (min-width: 650px) {
+        .sx-cart-data-options {
+            flex-direction: row;
+            width: 650px;
+        }
+    }
 
 </style>
